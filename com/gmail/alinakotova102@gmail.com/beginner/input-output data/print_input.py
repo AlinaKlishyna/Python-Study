ae060ef7cd9274bd1f7c1213787254c9b20eb7f3
Alina Klishyna <alinakotova102@gmail.com>
2026-01-27 14:37:56 +0100

    [be] feat: Add print/input chapter

---
+++ com/gmail/alinakotova102@gmail.com/beginner/input-output data/print_input.py
@@ -0,0 +1,60 @@
+print("Hello world!")
+
+
+def get_sum(a, b) -> int:
+    return a + b
+
+
+print(get_sum(5, 7))
+
+print("We studing Python!")
+
+# print() добавляет ровно один пробел между всеми своими аргументами
+print("We", "studing", "Pytnon", "!")
+
+print('В тексте есть "двойные" кавычки')
+print("В тексте есть 'одинарные' кавычки")
+
+print("I'm", "the", "BAD", "guy")
+
+print("Здравствуй, мир!")
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+print("******")
+print("*******")
+
+
+print("Как тебя зовут?")  # вывод текста
+print("Привет", input())
+
+# Команда print() выводит данные на экран
+# Команда input() считывает данные, введенные с клавиатуры
+
+variable_name = (
+    input()
+)  # input() получает от пользователя какие-то данные и подставляет строковое значение
+print("Entered text:", variable_name)
+
+city = "Rovereto"
+print(city, "è la mia città")
+
+name = "Alina"
+print("Mi chiamo", name, "e la mià città è", city)
+
+# Для именования переменных принято использовать стиль lower_case_with_underscores (слова из маленьких букв с подчеркиваниями). Например, следующие имена для переменных хорошие:
+# server_response
+# is_password_good
+# username
+# cyberpunk_77
+
+first = input()
+second = input()
+
+print("I am", first, "and", second)
+
+name = input()
+surname = input()
