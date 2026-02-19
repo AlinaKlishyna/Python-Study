cc4b59f12084b86ba2e51ee9e332011c3d5f6119
Alina Klishyna <alinakotova102@gmail.com>
2026-02-19 11:44:01 +0100

    [be] feat: OOP - add inheritance - doc for getter&setter

---
+++ com/gmail/alinakotova102@gmail.com/oop/inheritance/tasks.py
@@ -0,0 +1,75 @@
+class Appliance:
+    def __init__(self, model):
+        self.model = model
+        self.is_on = False
+
+    def turn_on(self):
+        self.is_on = True
+
+
+class Toaster(Appliance):
+    pass
+
+
+# ========================================================
+
+
+class Publication:
+    def __init__(self, title):
+        self.title = title
+
+
+class Book(Publication):
+    def get_author(self, author_name):
+        return f"Автор книги '{self.title}' - {author_name}"
+
+
+# ========================================================
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+class Student(Person):
+    def __init__(self, name, student_id):
+        super().__init__(name)
+        self.student_id = student_id
+
+
+# ========================================================
+
+
+class Shape:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+
+class Polygon(Shape):
+    def __init__(self, color, num_sides):
+        super().__init__(color)
+        self.num_sides = num_sides
+
+
+class Square(Polygon):
+    def __init__(self, color, side_length):
+        super().__init__(color, 4)
+        self.side_length = side_length
+
+
+# ========================================================
+
+
+class Worker:
+    def __init__(self, name, position):
+        self._name = name
+        self._position = position
+
+
+class HRManager(Worker):
+    def get_employee_info(self):
+        return f"Имя: {self._name}, Должность: {self._position}"
